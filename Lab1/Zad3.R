5a9f9b3305e54995883fde59dd6093a09ae7b869
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2017-10-19 16:37:40 +0200

    improved zad3 in lab1

--- Lab1/Zad3.R
+++ Lab1/Zad3.R
@@ -25,4 +25,27 @@ fitnessFunc <- function(x) {
     return(0) else return(-current_solution_survivalpoints)
 }
 
+
 #e
+GAmodel <- rbga.bin(size = 7, popSize = 200, iters = 100,
+                    mutationChance = 0.01, elitism = T, evalFunc = fitnessFunc)
+summary(GAmodel, echo=TRUE)
+
+#f
+iter<-100
+animate_plot <- function() {
+  for (i in seq(1, iter)) {
+    temp <- data.frame(Iteracja = c(seq(1, i), seq(1, i)), Legenda = c(rep("Średnia",
+                                                                           i), rep("Najlepsza", i)), WartoscFitness = c(-GAmodel$mean[1:i], -GAmodel$best[1:i]))
+    pl <- ggplot(temp, aes(x = Iteracja, y = WartoscFitness, group = Legenda,
+                           colour = Legenda)) + geom_line() + scale_x_continuous(limits = c(0,
+                                                                                            iter)) + scale_y_continuous(limits = c(0, 110)) + geom_hline(y =
+                                                                                                                                                           max(temp$WartoscFitness),
+                                                                                                                                                         lty = 2) + annotate("text", x = 1, y = max(temp$WartoscFitness) +
+                                                                                                                                                                               2, hjust = 0, size = 3, color = "black", label = paste("Najlepsze rozwiązanie:",
+                                                                                                                                                                                                                                      max(temp$WartoscFitness))) + scale_colour_brewer(palette = "Set1")
+    print(pl)
+  }
+}
+animate_plot()
+
