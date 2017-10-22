8347b1dea0c926d1d14cf274825ab907a6a206c7
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2017-10-22 13:48:03 +0200

    Lab1 homeworks complete

--- Lab1/Homework1.R
+++ Lab1/Homework1.R
@@ -1,9 +1,11 @@
+#a
 phi1 <- matrix(c(-1,2,4,-2,3,4,1,-3,4,1,-2,-4,2,-4,-4,-1,3,-4,1,2,3), ncol=3)
+#b
 phi2 <- as.matrix(read.csv("/home/marcin/Computing-Inteligence-R/Lab1/dubois20.cnf.txt", header=FALSE, skip=13, sep="", colClasses=c(NA,NA,NA,"NULL")))
-phi3 <- as.matrix(read.csv("/home/marcin/Computing-Inteligence-R/Lab1/aim-50-1_6-yes1-4.cnf.txt", header=FALSE, skip=13, sep="", colClasses=c(NA,NA,NA,"NULL")))
-fitness = function(substitution, phi){
-  counter = 0
-  result = FALSE
+#c
+fitness = function(temp, phi){
+  licz = 0
+  wynik = FALSE
   max = max(as.numeric(unlist(phi)))
   for(i in 1:nrow(phi)){
     row <- phi[i,]
@@ -13,17 +15,17 @@ fitness = function(substitution, phi){
     if(row_2 == 0){ row_2 = 3}
     row_3 <- abs(row[3])%%3
     if(row_3 == 0){ row_3 = 3}
-    x1 = substitution[row_1]
+    x1 = temp[row_1]
     if(row[1] < 0){ x1 = !x1 }
-    x2 = substitution[row_2]
+    x2 = temp[row_2]
     if(row[2] < 0){ x2 = !x2 }
-    x3 = substitution[row_3]
+    x3 = temp[row_3]
     if(row[3] < 0){ x3 = !x3 }
-    result = x1 | x2 | x3
-    if(!is.na(result)){ if(result){ counter = counter + 1 } }
+    wynik = x1 | x2 | x3
+    if(!is.na(wynik)){ if(wynik){ licz = licz + 1 } }
   }
-  return(counter)
+  return(licz)
 }
-
-counter = fitness(c(1,0,1,1), phi2)
+#d
+licz = fitness(c(1,0,1,1), phi2)
 
--- Lab1/Homework2.R
+++ Lab1/Homework2.R
@@ -3,11 +3,11 @@ library(ggplot2)
 
 phi1 <- matrix(c(-1,2,4,-2,3,4,1,-3,4,1,-2,-4,2,-4,-4,-1,3,-4,1,2,3), ncol=3)
 phi2 <- as.matrix(read.csv("/home/marcin/Computing-Inteligence-R/Lab1/dubois20.cnf.txt", header=FALSE, skip=13, sep="", colClasses=c(NA,NA,NA,"NULL")))
-phi3 <- as.matrix(read.csv("/home/marcin/Computing-Inteligence-R/Lab1/aim-50-1_6-yes1-4.cnf.txt", header=FALSE, skip=13, sep="", colClasses=c(NA,NA,NA,"NULL")))
-fitnessFunc <- function(substitution) {
+
+fitnessFunc <- function(temp) {
   phi = phi2
-  counter = 0
-  result = FALSE
+  licz = 0
+  wynik = FALSE
   max = max(as.numeric(unlist(phi)))
   for(i in 1:nrow(phi)){
     row <- phi[i,]
@@ -17,16 +17,16 @@ fitnessFunc <- function(substitution) {
     if(row_2 == 0){ row_2 = 3}
     row_3 <- abs(row[3])%%3
     if(row_3 == 0){ row_3 = 3}
-    x1 = substitution[row_1]
+    x1 = temp[row_1]
     if(row[1] < 0){ x1 = !x1 }
-    x2 = substitution[row_2]
+    x2 = temp[row_2]
     if(row[2] < 0){ x2 = !x2 }
-    x3 = substitution[row_3]
+    x3 = temp[row_3]
     if(row[3] < 0){ x3 = !x3 }
-    result = x1 | x2 | x3
-    if(!is.na(result)){ if(result){ counter = counter + 1 } }
+    wynik = x1 | x2 | x3
+    if(!is.na(wynik)){ if(wynik){ licz = licz + 1 } }
   }
-  return(-counter)
+  return(-licz)
 }
 
 GAmodel <- rbga.bin(size = 7, popSize = 200, iters = 200,
