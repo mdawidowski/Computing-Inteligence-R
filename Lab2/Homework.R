4d879ba6cf70be4c6bb193a2f701491f9774d8f7
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2017-11-05 13:57:32 +0100

    Homework color changes

--- Lab2/Homework.R
+++ Lab2/Homework.R
@@ -7,7 +7,7 @@ iris.pca.data <- predict(iris.pca)
 iris.pca.data <- iris.pca.data[,-3]
 #
 Species <- data.matrix(iris[,5])
-cols <- c('red', 'blue', 'green')
+cols <- c("green", 'blue', 'orange')
 with(iris, plot(iris.pca.data, col=cols[Species], pch=as.numeric(Species)))
 legend("topleft", c("setosa", "versicolor", "virginica"), fill=cols)
 #
@@ -36,10 +36,10 @@ recognizeIris <- function(SeplLength,SepalWidth,PetalLength,PetalWidth) {
   PC1 = iris.pca[2]$rotation[,1] %*% wektor
   PC2 = iris.pca[2]$rotation[,2] %*% wektor
   
-  points(PC1, PC2)
+  points(PC1, PC2, pch=16)
 }
 
 recognizeIris(6.9, 3.2, 5.6, 2.2) # virginica
-recognizeIris(6.0, 2.6, 4.4, 1.6) # versicolor lub virginica
+recognizeIris(6.0, 2.6, 4.4, 1.6) # virginica lub versicolor 
 recognizeIris(4.8, 3.6, 1.4, 0.2) # setosa
 
