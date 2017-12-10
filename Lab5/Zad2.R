325b555e43b2fe557cedeb402f6e9c7905cf42a2
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2017-12-10 13:19:51 +0100

    created Lab6

--- Lab5/Zad2.R
+++ Lab5/Zad2.R
@@ -1,5 +1,10 @@
-iris.numeric <- as.matrix(iris[,-5], col.names=F)
-iris.log <- log(iris.numeric)
+iris <- data.matrix(iris[,-5])
+colnames(iris) <- NULL
+iris.log <- log(iris)
 iris.preproc <- scale(iris.log)
 iris.pca <- prcomp(iris.preproc)
 iris.pca.data <- predict(iris.pca)[,1:2]
+iris.kmeans <- kmeans(iris.pca.data, 3)
+plot(iris.pca.data, col = iris.kmeans$cluster)
+points(iris.kmeans$centers, col = 1:3, pch = 3, cex = 2)
+
